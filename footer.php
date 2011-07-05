4295b6ae6b5a9815621fbd66814f0d5c7ef0d816
Craig Cabrey <craigcabrey@gmail.com>
2011-07-04 22:59:20 -0400

    simplified the footer and added better attribution to original author

--- footer.php
+++ footer.php
@@ -3,25 +3,20 @@
 <div id="footer">
 
   <!-- Footer Links -->
-  
-  <h5>Elsewhere</h5>
   <ul class="elsewhere">
     <?php wp_list_bookmarks('categorize=0&title_li=&category_name=footer&show_images=0'); ?>
   </ul>
 
   <!-- Search Field -->
-  
   <div class="footerContent">
     <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-      <div id="search">
-        <input type="text" value="<?php the_search_query(); ?>" name="s" id="s" />
-        <input type="submit" id="searchsubmit" value="Search" />
-      </div>
+      <input type="text" value="<?php the_search_query(); ?>" name="s" id="s" />
+      <input type="submit" id="searchsubmit" value="Search" /> 
     </form>
     
-    
-    <p>&copy; <?php bloginfo('name'); ?>. Powered by <a href="http://wordpress.org/">WordPress</a> and <a href="http://jimbarraud.com/manifest/">Manifest</a></p>
+    <p>&copy; <?php bloginfo('name'); ?> | <a href="http://jimbarraud.com/manifest/">Manifest</a> by <a href="http://jimbarraud.com/">Jim Barraud</a></p>
   </div>
+
 </div>
 
 <?php wp_footer(); ?>
