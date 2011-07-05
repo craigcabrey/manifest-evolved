0bd7503767eb1330084fa3b9ea71026b7f03956f
Craig Cabrey <craigcabrey@gmail.com>
2011-07-05 12:51:25 -0400

    footer adjustments

--- footer.php
+++ footer.php
@@ -8,13 +8,14 @@
   </ul>
 
   <!-- Search Field -->
-  <div class="footerContent">
+  <div class="footerSearch">
     <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
       <input type="text" value="<?php the_search_query(); ?>" name="s" id="s" />
       <input type="submit" id="searchsubmit" value="Search" /> 
     </form>
+  </div>
 
-    <br>    
+  <div class="footerContent">
     <p>&copy; <?php bloginfo('name'); ?> | <a href="http://jimbarraud.com/manifest/">Manifest</a> by <a href="http://jimbarraud.com/">Jim Barraud</a></p>
   </div>
 
