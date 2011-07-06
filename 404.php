c9e7e8c6bd543ec1e4d04995c54bcdc5144c1a02
Craig Cabrey <craigcabrey@gmail.com>
2011-07-05 20:22:42 -0400

    404, archive, and archives coding style changes

--- 404.php
+++ 404.php
@@ -1,7 +1,6 @@
 <?php get_header(); ?>
 
-<div id="coreContent">
-
+  <div id="coreContent">
 
     <div class="post hentry single">
       <div class="postContent">
@@ -10,28 +9,20 @@
           <p>Unfortunately the content you're looking for isn't here. There may be a misspelling in your web address or you may have clicked a link for content that no longer exists. Perhaps you would be interested in our most recent articles.</p>
         </div>
       </div>
-   </div>
+    </div>
 
-
-   <h4>Recent Articles</h4>
-
-   <?php query_posts('cat=&showposts=5'); ?>
-   <ul id="recentPosts">
-
-   		<?php while (have_posts()) : the_post(); ?>
-
-      <li>
-        <a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
-        <div class="postDate"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php the_date('F j, Y') ?></abbr></div>
-      </li>
-
-   		<?php endwhile; ?>
-   
+    <h4>Recent Articles</h4>
+    <?php query_posts('cat=&showposts=5'); ?>
+    <ul id="recentPosts">
+      <?php while (have_posts()) : the_post(); ?>
+        <li>
+          <a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
+          <div class="postDate"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php the_date('F j, Y') ?></abbr></div>
+        </li>
+      <?php endwhile; ?>
     </ul>
-   	</div>
+  </div>
 
 </div>
-
-
 
 <?php get_footer(); ?>
