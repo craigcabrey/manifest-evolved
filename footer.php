94997a084442d189869c4ee9422c392387cb7ca6
Craig Cabrey <craigcabrey@gmail.com>
2011-07-05 22:25:07 -0400

    coding style modifications

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
 </div>
 
 <div id="footer">
-
   <!-- Footer Links -->
   <ul class="elsewhere">
     <?php wp_list_bookmarks('categorize=0&title_li=&category_name=footer&show_images=0'); ?>
@@ -11,15 +10,15 @@
   <div class="footerSearch">
     <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
       <input type="text" value="<?php the_search_query(); ?>" name="s" id="s" />
-      <input type="submit" id="searchsubmit" value="Search" /> 
+      <input type="submit" id="searchsubmit" value="Search" />
     </form>
   </div>
 
+  <!-- Author & Theme info -->
   <ul class="footerContent">
     <li>&copy; <?php bloginfo('name'); ?></li>
     <li><a href="http://jimbarraud.com/manifest/">Manifest</a> by <a href="http://jimbarraud.com/">Jim Barraud</a></li>
   </ul>
-
 </div>
 
 <?php wp_footer(); ?>
--- single.php
+++ single.php
@@ -1,16 +1,13 @@
 <?php get_header(); ?>
 
-<div id="coreContent">
-
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-
+  <div id="coreContent">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
       <div class="post single hentry">
         <div class="postContent">
           <h3 class="entry-title"><?php the_title(); ?></h3>
           <h4 class="vcard author">by <span class="fn"><?php the_author(); ?></span></h4>
           <div class="entry-content">
-              <?php the_content(); ?>
+            <?php the_content(); ?>
           </div>
         </div>
         <div class="postMeta">
@@ -19,43 +16,44 @@
           $arc_month = get_the_time('m');
           $arc_day = get_the_time('d');
           ?>
-        
-          <div class="postDate"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO'); ?>"><a href="<?php echo get_day_link("$arc_year", "$arc_month", 
-          "$arc_day"); ?>"><?php the_time('F j, Y'); ?></a></abbr></div>
-        	<div class="categories"><span>Under:</span> <?php the_category(', '); ?></div>
-        	<?php the_tags('<span>Tags:</span> ', ' : ', ''); ?>
-        	
-        	
+
+          <div class="postDate">
+            <abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO'); ?>">
+              <a href="<?php echo get_day_link("$arc_year", "$arc_month", "$arc_day"); ?>"><?php the_time('F j, Y'); ?></a>
+            </abbr>
+          </div>
+          <div class="categories">
+            <span>Under:</span>
+            <?php the_category(', '); ?>
+          </div>
+          <?php the_tags('<span>Tags:</span> ', ' : ', ''); ?>
 
         </div>
       </div>
 
-      <?php 
-      /*
-      <div class="googleAd">
-        
-        <!-- You Ad Code Here -->
+    <?php 
+    /* <div class="googleAd">
+       <!-- You Ad Code Here -->
+       </div> */
+    ?>
 
+    <?php comments_template(); ?>
+
+    <div class="pageNav">
+      <div class="prev">
+        <?php previous_post_link('%link', '&laquo; Previous Post'); ?>
       </div>
-      */
-      ?>
-      
-	<?php comments_template(); ?>
+      <div class="next">
+        <?php next_post_link('%link', 'Next Post &raquo;') ?>
+      </div>
+    </div>
 
-  <div class="pageNav">
-    <div class="prev"><?php previous_post_link('%link', '&laquo; Previous Post'); ?></div>
-    <div class="next"><?php next_post_link('%link', 'Next Post &raquo;') ?></div>
+    <?php endwhile; else: ?>
+    <p>Sorry, no posts matched your criteria.</p>
+    <?php endif; ?>
+
   </div>
 
-
-	<?php endwhile; else: ?>
-
-		<p>Sorry, no posts matched your criteria.</p>
-
-<?php endif; ?>
-
-</div>
 </div>
 
 <?php get_footer(); ?>
-
